551d3c8878ae36b832f7163d3470f9214c242652
vanyusha82228 <ivan00.02@mail.ru>
2022-10-14 19:27:42 +0300

    task 2

--- HM1/Program.cs
+++ HM1/Program.cs
@@ -1,9 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-
-
-// Task 1  Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+﻿// Task 1  Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
 /*
 Console.Write("Input first number ");
@@ -24,3 +19,27 @@ else
 
 */
 
+// Task 2   Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+/*
+Console.Write("Input int first number ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input  int second number ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input  int third number ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+int max = number1 ;
+
+if (number2 > max)
+{
+    max= number2;
+}
+if (number3 > max)
+{
+    max= number3;
+}
+
+Console.WriteLine($"The largest number {max}");
+
+*/
