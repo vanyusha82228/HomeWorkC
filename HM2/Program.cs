f1f553914d6428b5adaa92b715399f7d639362ad
vanyusha82228 <ivan00.02@mail.ru>
2022-10-19 18:58:22 +0300

    task 1

--- HM2/Program.cs
+++ HM2/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿// Задание 1.Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int SecondDigit(int num)
+{
+    int des = num/10;
+    int Secondnumber = des %10;
+    return Secondnumber;
+}
+
+
+Console.Write("Iput number of threes: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Second digit of the number {number} is {SecondDigit(number)}");
