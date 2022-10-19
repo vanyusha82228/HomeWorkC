6059e820fa87436041f2c09ed6459ac656c79584
vanyusha82228 <ivan00.02@mail.ru>
2022-10-19 19:12:20 +0300

    task 3

--- HM2/Program.cs
+++ HM2/Program.cs
@@ -1,5 +1,7 @@
 ﻿// Задание 1.Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
+
+/*
 int SecondDigit(int num)
 {
     int des = num/10;
@@ -12,3 +14,36 @@ Console.Write("Iput number of threes: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine($"Second digit of the number {number} is {SecondDigit(number)}");
+*/
+
+
+// Задание2. Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+
+
+
+
+
+// Задание3. Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+/*
+bool DayOfTheWeek(int num)
+{
+    if (num >5 && num < 8)
+    {
+        Console.WriteLine("It's the weekend");
+        return true;
+    }
+    else
+    {
+        Console.WriteLine("it's not a day off");
+        return false;
+    }
+}
+
+Console.Write("Iput the number of the day of the week: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(DayOfTheWeek(number));
+*/
+
+
