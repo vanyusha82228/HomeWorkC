e8a11dae67eed48ee21030e754e3024976cc9356
vanyusha82228 <ivan00.02@mail.ru>
2022-10-20 17:42:10 +0300

    task 3 apgreid

--- HM2/Program.cs
+++ HM2/Program.cs
@@ -18,17 +18,51 @@ Console.WriteLine($"Second digit of the number {number} is {SecondDigit(number)}
 
 
 // Задание2. Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+/*
+int Dev(int num)
+{
+    if (num > 99 && num < 1000)
+    {
+        int third = num % 10;
+        return third;
+    }
+    if (num > 0 && num < 100)
+    {
+        int third = -1;
+        return third;
+    }
+    if (num > 999 && num < 10000)
+    {
+        int fourth = num / 10;
+        int third = fourth %10;
+        return third;
+    }
+    if (num > 9999 && num < 100000)
+    {
+        int fifth = num / 100;
+        int third = fifth %10;
+        return third;
+    }
+}
 
+Console.Write("Iput the number: ");
+int number = Convert.ToInt32(Console.ReadLine());
 
-
-
+Console.WriteLine(Dev(number));
+*/
 
 // Задание3. Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 
-/*
+
 bool DayOfTheWeek(int num)
 {
-    if (num >5 && num < 8)
+    if (num >=8 || num<= 0)
+    {
+        Console.WriteLine("invalid number");
+        return false;
+    }
+    
+    if (num >=5 && num <= 7)
     {
         Console.WriteLine("It's the weekend");
         return true;
@@ -38,12 +72,14 @@ bool DayOfTheWeek(int num)
         Console.WriteLine("it's not a day off");
         return false;
     }
+
+
 }
 
 Console.Write("Iput the number of the day of the week: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine(DayOfTheWeek(number));
-*/
+
 
 
