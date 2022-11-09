a20ce60235e4a8ff57980113d7998786e3c054bc
vanyusha82228 <ivan00.02@mail.ru>
2022-11-09 18:49:40 +0300

    task 2

--- HM4/Program.cs
+++ HM4/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Задание 1. Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
-
+/*
 int Stepen(int num1, int num2)
 {
     int res = 1;
@@ -17,8 +17,28 @@ Console.Write("Input desired degree : ");
 int number2 = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine($"if you raise a number {number} to the power {number2} you get {Stepen(number,number2)}");
+*/
+
+
 
 // Задание2. Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+int SumNumbers(int num)
+{
+    int res =0 ;
+    while(num!=0)
+    {
+        int des = num%10;
+        res = res+ des;
+        num= num/10;
+    }
+    return res;
+}
+
+Console.Write("Input number : ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"the sum of the digits {number} of the number is {SumNumbers(number)}");
+
 
 
 // Задание3. Напишите программу, которая задаёт массив из m элементов и выводит их на экран
