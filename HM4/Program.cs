fa8a5356b98171cf938c8c2d1b1c820c2457f810
vanyusha82228 <ivan00.02@mail.ru>
2022-11-09 18:57:24 +0300

    task 3

--- HM4/Program.cs
+++ HM4/Program.cs
@@ -22,6 +22,7 @@ Console.WriteLine($"if you raise a number {number} to the power {number2} you ge
 
 
 // Задание2. Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+/*
 int SumNumbers(int num)
 {
     int res =0 ;
@@ -38,7 +39,35 @@ Console.Write("Input number : ");
 int number = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine($"the sum of the digits {number} of the number is {SumNumbers(number)}");
-
+*/
 
 
 // Задание3. Напишите программу, которая задаёт массив из m элементов и выводит их на экран
+int[] CreateRandomArreay(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+
+    for(int i=0 ; i<size; i++)
+        array[i] = new Random().Next(minValue,maxValue+1);
+
+    return  array;
+}
+
+void ShowArray(int[] array)
+{
+    for(int i=0; i<array.Length; i++)
+        Console.Write(array[i]+ " ");
+
+    Console.WriteLine();
+}
+
+Console.Write("input number of elements: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("enter the minimum value in the array: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("enter the Maximum value in the array: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+
+int [] Array = CreateRandomArreay(n, min, max);
+ShowArray(Array);
