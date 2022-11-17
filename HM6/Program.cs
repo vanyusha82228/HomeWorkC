e36ca630ee0873991e08e6f43a291187670b3655
vanyusha82228 <ivan00.02@mail.ru>
2022-11-17 17:56:11 +0300

    task2

--- HM6/Program.cs
+++ HM6/Program.cs
@@ -1,4 +1,5 @@
 ﻿// Задача1.Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+/*
 int EnteringnNumbers(int countMum)
 {
     int count = 0;
@@ -16,6 +17,30 @@ int EnteringnNumbers(int countMum)
 Console.Write("Enter count of numbers: ");
 int m = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine($"number of positive numbers= {EnteringnNumbers(m)}");
-
+*/
 
 // Задача2.Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+
+string lineIntersection(double k1, double b1, double k2, double b2)
+{
+    if(k1==k2) return "intersect";
+    if (b1==b2) return "parallel";
+    
+    double x= (b2-b1)/(k1-k2);
+    double y= k1*x+b1;
+    return $"point of intersection of two lines{(x,y)}";
+    
+}
+
+
+Console.Write("Enter k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine(lineIntersection(k1,b1,k2,b2));
