6225482909ea395ec10bd3b934318060818a454c
vanyusha82228 <ivan00.02@mail.ru>
2022-11-23 18:34:38 +0300

    task 3

--- HM7/Program.cs
+++ HM7/Program.cs
@@ -42,7 +42,7 @@ ShowInt2dArray(myArray);
 
 */
 //Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
-
+/*
 int[,] CreateRandom2dArray(int rows, int colums, int minValue, int maxValue)
 {
     int[,] array = new int[rows, colums];
@@ -93,7 +93,67 @@ Console.Write("input posColums: ");
 int pos2 = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine(FindValue(myArray, pos1, pos2));
-
+*/
 
 //Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
 
+int[,] CreateRandom2dArray(int rows, int colums, int minValue, int maxValue)
+{
+    int[,] array = new int[rows, colums];
+
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < colums; j++)
+            array[i, j] = new Random().Next(minValue, maxValue);
+    return array;
+}
+
+void Show2dArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i, j] + " ");
+
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+double[] AverageColum(int[,] array)
+{
+    double[] newArray= new double[array.GetLength(1)];
+    for(int i=1; i<array.GetLength(0);i++)
+    {
+        double sum=0;
+        for(int j=0;j<array.GetLength(1); j++)
+            sum+= array[i,j];
+
+        double average= sum/array.GetLength(0);
+        newArray[i]=average;
+    }
+    return newArray;
+}
+
+void ShowArray(double[] array) 
+{
+    for(int i=0; i<array.Length; i++)
+        Console.Write(array[i]+ " ");
+
+    Console.WriteLine();
+}
+
+
+
+Console.Write("input number of ROWS: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("input number of COLUMS: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("input min posibel value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("input max posiber value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[,] myArray = CreateRandom2dArray(m, n, min, max);
+Show2dArray(myArray);
+
+ShowArray(AverageColum(myArray));
