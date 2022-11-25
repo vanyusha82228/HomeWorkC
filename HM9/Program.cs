d3b1028767ea7d4882ecd061eada994ee3b093f4
vanyusha82228 <ivan00.02@mail.ru>
2022-11-25 18:59:06 +0300

    task 2

--- HM9/Program.cs
+++ HM9/Program.cs
@@ -1,4 +1,5 @@
 ﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+/*
 void ShowNums(int n)
 {
     Console.Write(n+" ");
@@ -6,8 +7,21 @@ void ShowNums(int n)
 }
 
 ShowNums(5);
-
+*/
 
 //  Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int SumElements(int m, int n)
+{
+    if(m>n) 
+        return SumElements(m,n+1)+m;
+    else if(m<n)
+        return SumElements(m+1,n)+m;
+    else
+        return m;
+    
+}
+
+Console.WriteLine(SumElements(1,5));
 
 // Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
