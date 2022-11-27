5ab516918f223b23978a0e4e8b2e7aaa79112b8d
vanyusha82228 <ivan00.02@mail.ru>
2022-11-27 17:17:22 +0300

    task 2

--- HM8/Program.cs
+++ HM8/Program.cs
@@ -1,4 +1,5 @@
 ﻿//Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива
+/*
 int[,] CreateRandom2dArray(int rows, int colums, int minValue, int maxValue)
 {
     int[,] array = new int[rows, colums];
@@ -52,9 +53,70 @@ Show2dArray(myArray);
 
 SortArray(myArray);
 Show2dArray(myArray);
+*/
 
 //Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+int[,] CreateRandom2dArray(int rows, int colums, int minValue, int maxValue)
+{
+    int[,] array = new int[rows, colums];
 
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < colums; j++)
+            array[i, j] = new Random().Next(minValue, maxValue);
+    return array;
+}
+
+void Show2dArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i, j] + " ");
+
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+
+int[] SumRowsArray(int[,] array)
+{
+    int[] sumArray = new int[array.GetLength(0)];
+    for(int i=0; i<array.GetLength(0);i++)
+    {
+        int sum = 0;
+            for(int j=0; j<array.GetLength(1);j++)
+                sum+=array[i,j];
+        sumArray[i]=sum;
+    }
+    return sumArray;
+}
+
+int ShowMinSumArray(int[,] array)
+{
+    int min = 0;
+    int[] minRowsSum = SumRowsArray(array); 
+    for(int i=0;i<minRowsSum.Length;i++)
+        if(minRowsSum[i]<minRowsSum[min])
+            min=i;
+    return min;
+
+}
+
+
+Console.Write("input number of ROWS: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("input number of COLUMS: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("input min posibel value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("input max posiber value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[,] myArray = CreateRandom2dArray(m, n, min, max);
+Show2dArray(myArray);
+
+Console.WriteLine(ShowMinSumArray(myArray));
 
 //Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
 
