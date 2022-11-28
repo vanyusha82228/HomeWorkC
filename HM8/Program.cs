c23c90e20ff4718f1fdeead84cc9a099ca1c93d8
vanyusha82228 <ivan00.02@mail.ru>
2022-11-28 19:31:01 +0300

    task 3

--- HM8/Program.cs
+++ HM8/Program.cs
@@ -120,6 +120,7 @@ Show2dArray(myArray);
 Console.WriteLine(ShowMinSumArray(myArray));
 */
 //Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+/*
 int[,] CreateRandom2dArray(int rows, int colums, int minValue, int maxValue)
 {
     int[,] array = new int[rows, colums];
@@ -167,8 +168,6 @@ int max1 = Convert.ToInt32(Console.ReadLine());
 int[,] myArray1 = CreateRandom2dArray(m1, n1, min1, max1);
 Show2dArray(myArray1);
 
-Console.WriteLine("");
-
 Console.Write("input number of ROWS for the second: ");
 int m2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("input number of COLUMS for the second: ");
@@ -182,8 +181,64 @@ int[,] myArray2 = CreateRandom2dArray(m2, n2, min2, max2);
 Show2dArray(myArray2);
 
 Show2dArray(MatricesProduct(myArray1,myArray2));
-
+*/
 //Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+int[] UniqueArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+    Random rand = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rand.Next(minValue, maxValue);
+        if (i != 0)
+        {
+            for (int j = 0; j < i; j++)
+                while (array[j] == array[i])
+                {
+                    array[i] = rand.Next(minValue, maxValue + 1);
+                    j = 0;
+                }
+        }
+    }
+    return array;
+}
 
+int[,,] Unique3DArray(int xSize=2, int ySize=2, int zSize=2, int minValue=10, int maxValue=99)
+{
+    int[,,] array = new int[xSize, ySize, zSize];
+    int[] baseArray = UniqueArray(xSize * ySize * zSize, minValue, maxValue);
+    int baseIndex = 0;
+    for (int i = 0; i < xSize; i++)
+        for (int j = 0; j < ySize; j++)
+            for (int k = 0; k < zSize; k++)
+            {
+                array[i, j, k] = baseArray[baseIndex];
+                baseIndex++;
+            }
+    return array;
+}
+void Show3DArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            string output = string.Empty;
+            for (int k = 0; k < array.GetLength(2); k++)
+                output += $"{array[i, j, k]}({i},{j},{k}) ";
+            Console.WriteLine(output);
+        }
+        Console.WriteLine();
+    }
+}
+Console.WriteLine("Enter three dimensions of the array (x * y * z <= 90)");
+Console.Write("Enter x dimension of the array: ");
+int arrX = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter y dimension of the array: ");
+int arrY = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter z dimension of the array: ");
+int arrZ = Convert.ToInt32(Console.ReadLine());
+int[,,] myArray = Unique3DArray(arrX, arrY, arrZ);
+Show3DArray(myArray);
 
 //Напишите программу, которая заполнит спирально массив 4 на 4.
